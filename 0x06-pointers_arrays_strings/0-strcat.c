90cff4695322e011a1d42d4595674a7d84116cf1
Kosiso60 <obinwakosiso@gmail.com>
2022-04-24 21:35:23 +0100

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
-include "main.h"
+#include "main.h"
 
 /**
- *_strcat - concatinate 2 strings.
- *@dest: first string.
- *@src: second string.
- *Return: string.
+ * _strcat - concatinate 2 strings.
+ * @dest: first string.
+ * @src: second string.
+ * Return: string.
  */
 
 char *_strcat(char *dest, char *src)
